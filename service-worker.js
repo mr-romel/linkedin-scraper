25fd9d9f27ce32facd832f66b957bd6b88501ec6
mr-romel <mahmoud.khyrat89@gmail.com>
2026-09-11 01:14:42 +0300

    Update PWA cache for current lead pipeline modules

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='khyrat-leads-v2';const ASSETS=['./app.html','./app.js','./core.js','./transport.js','./manifest.json'];
+const CACHE='khyrat-leads-v3';const ASSETS=['./app.html','./app.js','./core.js','./discovery.js','./ingest.js','./queue.js','./analytics.js','./transport.js','./manifest.json','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{const y=x.clone();caches.open(CACHE).then(c=>c.put(e.request,y));return x}))));
