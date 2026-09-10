03153ffc8499b8ff6d1136a040b9745bdb329a37
mr-romel <mahmoud.khyrat89@gmail.com>
2026-09-10 23:44:03 +0300

    Cache transport layer and rotate PWA cache

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,4 @@
-const CACHE='khyrat-leads-v1';const ASSETS=['./app.html','./app.js','./core.js','./manifest.json'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+const CACHE='khyrat-leads-v2';const ASSETS=['./app.html','./app.js','./core.js','./transport.js','./manifest.json'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{const y=x.clone();caches.open(CACHE).then(c=>c.put(e.request,y));return x}))));
