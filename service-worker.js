2362a7d4fd50cd09cd744fc1ffb3e17f0f518d04
mr-romel <mahmoud.khyrat89@gmail.com>
2026-09-10 23:30:26 +0300

    Add offline PWA service worker

---
+++ service-worker.js
@@ -0,0 +1,3 @@
+const CACHE='khyrat-leads-v1';const ASSETS=['./app.html','./app.js','./core.js','./manifest.json'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{const y=x.clone();caches.open(CACHE).then(c=>c.put(e.request,y));return x}))));
